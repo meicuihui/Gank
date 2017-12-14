022ddddc1e086f94e28e3cf936172e4593863b63
2032730677@qq.com <2032730677@qq.com>
2017-12-14 20:35:52 +0800

    fix1

--- app/src/main/java/ye/android/com/gank/api/Api.java
+++ app/src/main/java/ye/android/com/gank/api/Api.java
@@ -7,6 +7,7 @@ package ye.android.com.gank.api;
 
 public class Api {
     public static String getData(String type,String count,String page){
+        
         return "http://gank.io/api/data/"+type+"/"+count+"/"+page;
     }
 }
